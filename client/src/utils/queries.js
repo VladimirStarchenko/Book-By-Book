2aa221f2339e91de71aa948efe0f12eb35ea9640
VladimirStarchenko <91164950+VladimirStarchenko@users.noreply.github.com>
2022-03-23 21:38:45 -0400

    added queries

---
+++ client/src/utils/queries.js
@@ -0,0 +1,23 @@
+// This will hold the query GET_ME,
+//which will execute the me query set up using Apollo Server.
+
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        title
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
+`;
